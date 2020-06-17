fff11a109f3c284a046319848b422bba2439c2a8
Catj99 <cristian.terrones@hotmail.com>
2020-06-17 00:46:57 -0500

    Se realizo la creacion y el añadido del background del splash.

--- app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/BlueActivity.java
+++ app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/BlueActivity.java
@@ -10,6 +10,6 @@ public class BlueActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue);
-        //hol
+
     }
 }
