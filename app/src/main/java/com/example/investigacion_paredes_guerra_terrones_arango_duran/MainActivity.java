dda65cc0b3ef4ea1dbe834f8bdc2f9a492dcf7d0
arangod98 <59971058+arangod98@users.noreply.github.com>
2020-06-16 21:42:53 -0500

    Se realizó la creació de tres activities adicionales. Dos para las funciones Bluetooth y Wifi; y una para la presentación de la aplicación (Estético).

--- app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/MainActivity.java
+++ app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.investigacion_paredes_guerra_terrones_arango_duran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,4 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Direcciona hacia la pantalla de Bluetooth.
+    public void blueTooth(View view){
+        Intent b = new Intent(getApplicationContext(), BlueActivity.class);
+        startActivity(b);
+    }
+
+    //Direcciona hacia la pantalla de Wifi.
+    public void wifiTest(View view){
+        Intent w = new Intent(getApplicationContext(), WifiActivity.class);
+        startActivity(w);
+    }
 }
---
+++ app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.example.investigacion_paredes_guerra_terrones_arango_duran;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
