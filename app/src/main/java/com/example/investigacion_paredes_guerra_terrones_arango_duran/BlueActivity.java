55a583ed7584733fe34be4c427ed7d0d23d2bc29
kendall2 <kguerra839@gmail.com>
2020-06-21 23:00:29 -0500

    Configuración de la activación del bluetooth

--- app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/BlueActivity.java
+++ app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/BlueActivity.java
@@ -2,14 +2,45 @@ package com.example.investigacion_paredes_guerra_terrones_arango_duran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class BlueActivity extends AppCompatActivity {
 
+    BluetoothAdapter bluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue);
+        bluetoothAdapter =BluetoothAdapter.getDefaultAdapter();
+    }
+    public void on(View v){
+        if(bluetoothAdapter == null){
+            Toast.makeText(getApplicationContext(),"Bluetooth no es soportado en este celular.",Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        {
+            if(!bluetoothAdapter.isEnabled())
+            {
+                Intent i= new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(i,1);
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode == 1)
+        {
+            if(resultCode == RESULT_OK){
+                Toast.makeText(getApplicationContext(),"Bluetooth es aceptado.",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void off(View v){
 
     }
 }
