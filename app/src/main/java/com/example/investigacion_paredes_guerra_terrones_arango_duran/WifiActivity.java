da79bc35f8f576101224103855c6f8ad8b201dee
arangod98 <59971058+arangod98@users.noreply.github.com>
2020-06-22 20:40:07 -0500

    Se agrego el permiso para cambiar el estado del wifi. Adicionalmente, se crearon los botones para encender y apagar la funcion de Wifi.

--- app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/WifiActivity.java
+++ app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/WifiActivity.java
@@ -2,13 +2,38 @@ package com.example.investigacion_paredes_guerra_terrones_arango_duran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class WifiActivity extends AppCompatActivity {
+    //Variables declaradas.
+    Button w_on, w_off;
+    WifiManager wifiM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi);
+        initializeControls();
+    }
+    //Metodo que inicia las variables.
+    public void initializeControls(){
+        w_on=(Button)findViewById(R.id.btn_turnon);
+        w_off=(Button)findViewById(R.id.btn_turnoff);
+        wifiM=(WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    }
+    //Metodo para encender el Wifi.
+    public void onWifi(View view){
+        wifiM.setWifiEnabled(true);
+        Toast.makeText(this, "Wifi Habilitado", Toast.LENGTH_LONG).show();
+    }
+    //Metodo para apagar el Wifi.
+    public void offWifi(View view){
+        wifiM.setWifiEnabled(true);
+        Toast.makeText(this, "Wifi Deshabilitado", Toast.LENGTH_LONG).show();
     }
 }
