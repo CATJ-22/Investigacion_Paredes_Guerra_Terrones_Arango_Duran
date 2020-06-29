b08da8333892b796e557fdce0e06adc0b8357306
Kiriam Paredes <41593879+Paredes02@users.noreply.github.com>
2020-06-28 20:27:04 -0500

    CORRECION (CREO) Del boton de de buscar nuevos dispositivos (version 2)

--- app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/BlueActivity.java
+++ app/src/main/java/com/example/investigacion_paredes_guerra_terrones_arango_duran/BlueActivity.java
@@ -1,7 +1,9 @@
 package com.example.investigacion_paredes_guerra_terrones_arango_duran;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
@@ -30,10 +32,10 @@ public class BlueActivity extends AppCompatActivity {
     public DeviceAdapter mDevicesAdapter;
     ListView lvNewDevices;
 
-    TextView mPairedTV,estado;
-    Button encendido,apagar,recibir;
+    TextView mPairedTV, estado;
+    Button encendido, apagar, recibir;
     BluetoothAdapter bluetoothAdapter;
-    Button btnBuscarDispositivo ;
+    Button btnBuscarDispositivo;
 
     //Broadcast Receivier para los dipsositovos que todavia no estan emparejados
     private BroadcastReceiver Receiverdevices = new BroadcastReceiver() {
@@ -41,11 +43,11 @@ public class BlueActivity extends AppCompatActivity {
         public void onReceive(Context context, Intent intent) {
             final String action = intent.getAction();
             Log.d(TAG, "onReceiver :ACTION FOUND");
-            if(action.equals(BluetoothDevice.ACTION_FOUND)){
+            if (action.equals(BluetoothDevice.ACTION_FOUND)) {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 mBTDevices.add(device);
-                Log.d(TAG,"ONRECIEVE" + device.getName() + ": " + device.getAddress());
-                mDevicesAdapter = new DeviceAdapter(context, R.layout.activity_device_list_adapter,mBTDevices);
+                Log.d(TAG, "ONRECIEVE" + device.getName() + ": " + device.getAddress());
+                mDevicesAdapter = new DeviceAdapter(context, R.layout.activity_device_list_adapter, mBTDevices);
                 lvNewDevices.setAdapter(mDevicesAdapter);
             }
         }
@@ -60,59 +62,57 @@ public class BlueActivity extends AppCompatActivity {
         IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
         registerReceiver(receiver, filter);
 
-        btnBuscarDispositivo =(Button)findViewById(R.id.btnBuscarDispositivos);
-        bluetoothAdapter =BluetoothAdapter.getDefaultAdapter();
-        estado =findViewById(R.id.statusBluetoothTv);
-        encendido =findViewById(R.id.onBtn);
-        apagar =findViewById(R.id.offBtn);
+        btnBuscarDispositivo = (Button) findViewById(R.id.btnBuscarDispositivos);
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        estado = findViewById(R.id.statusBluetoothTv);
+        encendido = findViewById(R.id.onBtn);
+        apagar = findViewById(R.id.offBtn);
         //Para buscar nuevos dispositivos
         lvNewDevices = findViewById(R.id.NewDevices);
         mBTDevices = new ArrayList<>();
         // Para emparejar nuevos dispositivos
-        recibir =findViewById(R.id.PairedBtn);
-        mPairedTV =findViewById(R.id.pairedTv);
+        recibir = findViewById(R.id.PairedBtn);
+        mPairedTV = findViewById(R.id.pairedTv);
         ArrayList<BluetoothDevice> arrayDevices;
-        encendido.setOnClickListener(new View.OnClickListener(){
+        encendido.setOnClickListener(new View.OnClickListener() {
             @Override
-                    public void onClick(View v){
-                if(!bluetoothAdapter.isEnabled()){
-                    Intent i= new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                    startActivityForResult(i,REQUEST_ENABLE_BT);
-                }
-                else {
-                    Toast.makeText(getApplicationContext(),"Bluetooth Encendido.",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                if (!bluetoothAdapter.isEnabled()) {
+                    Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(i, REQUEST_ENABLE_BT);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Bluetooth Encendido.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-       apagar.setOnClickListener(new View.OnClickListener(){
-           @Override
-           public void onClick(View v) {
-               if(!bluetoothAdapter.disable()){
-                   Intent i= new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                   startActivityForResult(i,REQUEST_DISCOVER_BT);
-               }
-               else {
-                   mPairedTV.setText("");
-                   Toast.makeText(getApplicationContext(),"Bluetooth Apagado.",Toast.LENGTH_SHORT).show();
-               }
-           }
+        apagar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!bluetoothAdapter.disable()) {
+                    Intent i = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                    startActivityForResult(i, REQUEST_DISCOVER_BT);
+                } else {
+                    mPairedTV.setText("");
+                    Toast.makeText(getApplicationContext(), "Bluetooth Apagado.", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
-       recibir.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               if(bluetoothAdapter.isEnabled()){
-                   mPairedTV.setText("Paired Devices");
-                   Set<BluetoothDevice> devices = bluetoothAdapter.getBondedDevices();
-                   for(BluetoothDevice device: devices) {
-                       mPairedTV.append("\nDevice: " + device.getName()+"," + device);
-                   }
-               }
-               else{
-                   Toast.makeText(getApplicationContext(),"Bluetooth activado emparejar dispositivo.",Toast.LENGTH_SHORT).show();
-               }
-           }
-       });
+        recibir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bluetoothAdapter.isEnabled()) {
+                    mPairedTV.setText("Paired Devices");
+                    Set<BluetoothDevice> devices = bluetoothAdapter.getBondedDevices();
+                    for (BluetoothDevice device : devices) {
+                        mPairedTV.append("\nDevice: " + device.getName() + "," + device);
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Bluetooth activado emparejar dispositivo.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -145,23 +145,40 @@ public class BlueActivity extends AppCompatActivity {
     }
 
     //BUSCAR DISPOSITIVOS
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public void BuscarDispositivos(View view) {
-    Log.d(TAG,"btnBuscarDispositivos: Looking for unpaired devices.");
-    if(bluetoothAdapter.isDiscovering()){
-        bluetoothAdapter.cancelDiscovery();
-        Log.d(TAG,"btnBuscarDispositivos: Canceling discovery");
-           //checkBTPermissions();
-        bluetoothAdapter.startDiscovery();
-        IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(Receiverdevices,discoverDevicesIntent);
-
+        Log.d(TAG, "btnBuscarDispositivos: Looking for unpaired devices.");
+        if (bluetoothAdapter.isDiscovering()) {
+            bluetoothAdapter.cancelDiscovery();
+            Log.d(TAG, "btnBuscarDispositivos: Canceling discovery");
+            checkBTPermissions();
+            bluetoothAdapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(Receiverdevices, discoverDevicesIntent);
+        }
+        if(!bluetoothAdapter.isDiscovering()){
+            checkBTPermissions();
+            bluetoothAdapter.startDiscovery();
+            IntentFilter discoverDevicesIntent = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(Receiverdevices, discoverDevicesIntent);
+        }
     }
-    }
-/*
-    private void checkBTPermissions(){
-        if(Build.VERSION)
 
-    }*/
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void checkBTPermissions() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            int permissionCheck = this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION");
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION");
+            if (permissionCheck != 0) {
+                this.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1001);
+            }
+        } else {
+            Log.d(TAG, "checkBTPermissions: o need to check permissions. SDK version < LOLLIPOP.");
+        }
+    }
+}
+
+
 
     // PAIR DEVICES
 /*
@@ -178,4 +195,3 @@ public class BlueActivity extends AppCompatActivity {
         }
     };
 */
-}
